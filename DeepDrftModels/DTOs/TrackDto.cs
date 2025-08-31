aac0004e03caa6c407613bf26c91930c0e26032b
daniel-c-harvey <work.daniel.harvey@gmail.com>
2025-08-30 21:43:15 -0400

    Initial DeepDrft Models Project  - Track Entity & DTO  - Paging models

---
+++ DeepDrftModels/DTOs/TrackDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DeepDrftModels.DTOs;
+
+public class TrackDto
+{
+    public long Id { get; set; }
+    public string MediaPath { get; set; }
+    public string TrackName { get; set; }
+    public string Artist { get; set; }
+    public string? Album { get; set; }
+    public string? Genre { get; set; }
+    public DateOnly? ReleaseDate { get; set; }
+    public string? ImagePath { get; set; }
+}
