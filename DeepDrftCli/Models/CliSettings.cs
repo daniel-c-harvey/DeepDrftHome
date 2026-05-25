82a310cf58b59c82ee03682b319f7881b885ec9e
Daniel Harvey <the.real.daniel.harvey@gmail.com>
2026-05-25 11:39:41 -0400

    remove DeepDrftCli project and CLI.sln; clean orphan GUID from DeepDrftHome.sln

--- DeepDrftCli/Models/CliSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace DeepDrftCli.Models
-{
-    public class CliSettings
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string VaultPath { get; set; } = string.Empty;
-    }
-}
